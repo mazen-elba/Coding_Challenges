d1cee984f6633459957c285ebe7571ae174aa3bf
Mazen Abdelbasir <mazen.elba@gmail.com>
2020-03-12 10:10:37 -0400

    Modified A-star.py

--- Search Algorithms/A-star.py
+++ Search Algorithms/A-star.py
@@ -35,25 +35,106 @@ class Node():
 
 # A-star Algorithm
 def astar(maze, start, end):
-	# create start and end node
-	start_node = Node(None, start)
-	start_node.g = start_node.h = start_node.f = 0
-	end_node = Node(None, end)
-	end_node.g = end_node.h = end_node.f = 0
+    # create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
 
-	# initialize both open and closed lists
-	open_list = []
-	closed_list = []
+    # initialize both open and closed list
+    open_list = []
+    closed_list = []
 
-	# add the start node
-	open_list.append(start_node)
+    # add the start node
+    open_list.append(start_node)
 
-	# loop until you find the end
-	while len(open_list) > 0:
-		# get current end
-		current_end = open_list[0]
-		current_index = 0
-		for index, item
+    # loop until you find the end
+    while len(open_list) > 0:
+
+        # get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # return reversed path
+
+        # generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # create new node
+            new_node = Node(current_node, node_position)
+
+            # append
+            children.append(new_node)
+
+        # loop through children
+        for child in children:
+            # child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # add the child to the open list
+            open_list.append(child)
+
+# --- Driver Program to Test Algorithm
+def main():
+
+    maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    start = (0, 0)
+    end = (7, 6)
+
+    path = astar(maze, start, end)
+    print(path)
+
+if __name__ == '__main__':
+    main()
 
 # --- Analysis
 # Time Complexity: O(E) worst-case; E = number of edges
